d8bef2193a581efa206ce460aded4269bb75b79a
Matt Maginniss <maginniss.matt@gmail.com>
2015-12-01 22:25:37 -0500

    Implemented new code for embedding text in an Image.

--- GroupGSteganography/Model/TextEmbeddor.cs
+++ GroupGSteganography/Model/TextEmbeddor.cs
@@ -1,4 +1,6 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
 
 namespace GroupGSteganography.Model
 {
@@ -53,125 +55,107 @@ namespace GroupGSteganography.Model
 
         public Image Embed()
         {
-            var state = State.Hiding;
+            var encodedImage = (Bitmap) this.SourceImage.Clone();
+            var markedImage = (Bitmap) this.SourceImage.Clone();
 
-            var bmp = new Bitmap(this.SourceImage);
+            this.encodeMessageInImage(encodedImage, markedImage, this.EmbeddedText);
 
-            var charIndex = 0;
-            var charValue = 0;
-            var pixelElementIndex = 0;
-
-            // holds the number of trailing zeros that have been added when finishing the process
-            var zeros = 0;
-
-            // hold pixel elements
-
-            // pass through the rows
-            for (var i = 0; i < bmp.Height; i++)
-            {
-                // pass through each row
-                for (var j = 0; j < bmp.Width; j++)
-                {
-                    // holds the pixel that is currently being processed
-                    var pixel = bmp.GetPixel(j, i);
-
-                    // now, clear the least significant bit (LSB) from each pixel element
-                    var red = pixel.R - pixel.R%2;
-                    var green = pixel.G - pixel.G%2;
-                    var blue = pixel.B - pixel.B%2;
-
-                    // for each pixel, pass through its elements (RGB)
-                    for (var n = 0; n < 3; n++)
-                    {
-                        // check if new 8 bits has been processed
-                        if (pixelElementIndex%8 == 0)
-                        {
-                            // check if the whole process has finished
-                            // we can say that it's finished when 8 zeros are added
-                            if (state == State.FillingWithZeros && zeros == 8)
-                            {
-                                // apply the last pixel on the image
-                                // even if only a part of its elements have been affected
-                                if ((pixelElementIndex - 1)%3 < 2)
-                                {
-                                    bmp.SetPixel(j, i, Color.FromArgb(red, green, blue));
-                                }
-
-                                // return the bitmap with the text hidden in
-                                return bmp;
-                            }
-
-                            // check if all characters has been hidden
-                            if (charIndex >= this.EmbeddedText.Length)
-                            {
-                                // start adding zeros to mark the end of the text
-                                state = State.FillingWithZeros;
-                            }
-                            else
-                            {
-                                // move to the next character and process again
-                                charValue = this.EmbeddedText[charIndex++];
-                            }
-                        }
-
-                        // check which pixel element has the turn to hide a bit in its LSB
-                        switch (pixelElementIndex%3)
-                        {
-                            case 0:
-                            {
-                                if (state == State.Hiding)
-                                {
-                                    // the rightmost bit in the character will be (charValue % 2)
-                                    // to put this value instead of the LSB of the pixel element
-                                    // just add it to it
-                                    // recall that the LSB of the pixel element had been cleared
-                                    // before this operation
-                                    red += charValue%2;
-
-                                    // removes the added rightmost bit of the character
-                                    // such that next time we can reach the next one
-                                    charValue /= 2;
-                                }
-                            }
-                                break;
-                            case 1:
-                            {
-                                if (state == State.Hiding)
-                                {
-                                    green += charValue%2;
-
-                                    charValue /= 2;
-                                }
-                            }
-                                break;
-                            case 2:
-                            {
-                                if (state == State.Hiding)
-                                {
-                                    blue += charValue%2;
-
-                                    charValue /= 2;
-                                }
-
-                                bmp.SetPixel(j, i, Color.FromArgb(red, green, blue));
-                            }
-                                break;
-                        }
-
-                        pixelElementIndex++;
-
-                        if (state == State.FillingWithZeros)
-                        {
-                            // increment the value of zeros until it is 8
-                            zeros++;
-                        }
-                    }
-                }
-            }
-
-            return bmp;
+            return markedImage;
         }
 
         #endregion
+
+        private void encodeMessageInImage(Bitmap bm, Bitmap visible_bm, string message)
+        {
+            // Initialize a random number generator.
+            var rand = new Random(1234);
+
+            // Create a new HashSet.
+            var usedPositions = new HashSet<string>();
+
+            // Encode the message length.
+            var bytes = BitConverter.GetBytes(message.Length);
+            for (var i = 0; i < bytes.Length; i++)
+            {
+                this.encodeByte(bm, visible_bm, rand, bytes[i], usedPositions);
+            }
+
+            // Encode the message.
+            var chars = message.ToCharArray();
+            for (var i = 0; i < chars.Length; i++)
+            {
+                this.encodeByte(bm, visible_bm, rand, (byte) chars[i], usedPositions);
+            }
+        }
+
+        private void encodeByte(Bitmap bm, Bitmap visibleBm, Random rand,
+            byte value, HashSet<string> usedPositions)
+        {
+            for (var i = 0; i < 8; i++)
+            {
+                // Pick a position for the ith bit.
+                int row, col, pix;
+                this.PickPosition(bm, rand, usedPositions, out row, out col, out pix);
+
+                // Get the color's pixel components.
+                var clr = bm.GetPixel(row, col);
+                var r = clr.R;
+                var g = clr.G;
+                var b = clr.B;
+
+                // Get the next bit to store.
+                var bit = 0;
+                if ((value & 1) == 1)
+                {
+                    bit = 1;
+                }
+
+                // Update the color.
+                switch (pix)
+                {
+                    case 0:
+                        r = (byte) ((r & 0xFE) | bit);
+                        break;
+                    case 1:
+                        g = (byte) ((g & 0xFE) | bit);
+                        break;
+                    case 2:
+                        b = (byte) ((b & 0xFE) | bit);
+                        break;
+                }
+                clr = Color.FromArgb(clr.A, r, g, b);
+                bm.SetPixel(row, col, clr);
+
+                // Display a red pixel.
+                visibleBm.SetPixel(row, col, Color.Red);
+
+                // Move to the next bit in the value.
+                value >>= 1;
+            }
+        }
+
+        private void PickPosition(Bitmap bm, Random rand,
+            HashSet<string> usedPositions,
+            out int row, out int col, out int pix)
+        {
+            for (;;)
+            {
+                // Pick random r, c, and pix.
+                row = rand.Next(0, bm.Width);
+                col = rand.Next(0, bm.Height);
+                pix = rand.Next(0, 3);
+
+                // See if this location is available.
+                var key =
+                    row + "/" +
+                    col + "/" +
+                    pix;
+                if (!usedPositions.Contains(key))
+                {
+                    usedPositions.Add(key);
+                    return;
+                }
+            }
+        }
     }
 }
