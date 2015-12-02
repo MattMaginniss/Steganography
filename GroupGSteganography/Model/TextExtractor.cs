0e64faf6e49e493bd1ef471350134b89470db23c
Matt Maginniss <maginniss.matt@gmail.com>
2015-12-01 22:39:04 -0500

    implemented code for the extraction of text.

--- GroupGSteganography/Model/TextExtractor.cs
+++ GroupGSteganography/Model/TextExtractor.cs
@@ -1,4 +1,7 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 
 namespace GroupGSteganography.Model
 {
@@ -37,92 +40,109 @@ namespace GroupGSteganography.Model
 
         public void Extract()
         {
-            var bmp = new Bitmap(this.EncodedImage);
-
-            var colorUnitIndex = 0;
-            var charValue = 0;
-
-            // holds the text that will be extracted from the image
-            var extractedText = string.Empty;
-
-            // pass through the rows
-            for (var i = 0; i < bmp.Height; i++)
-            {
-                // pass through each row
-                for (var j = 0; j < bmp.Width; j++)
-                {
-                    var pixel = bmp.GetPixel(j, i);
-
-                    // for each pixel, pass through its elements (RGB)
-                    for (var n = 0; n < 3; n++)
-                    {
-                        switch (colorUnitIndex%3)
-                        {
-                            case 0:
-                            {
-                                // get the LSB from the pixel element (will be pixel.R % 2)
-                                // then add one bit to the right of the current character
-                                // this can be done by (charValue = charValue * 2)
-                                // replace the added bit (which value is by default 0) with
-                                // the LSB of the pixel element, simply by addition
-                                charValue = charValue*2 + pixel.R%2;
-                            }
-                                break;
-                            case 1:
-                            {
-                                charValue = charValue*2 + pixel.G%2;
-                            }
-                                break;
-                            case 2:
-                            {
-                                charValue = charValue*2 + pixel.B%2;
-                            }
-                                break;
-                        }
-
-                        colorUnitIndex++;
-
-                        // if 8 bits has been added,
-                        // then add the current character to the result text
-                        if (colorUnitIndex%8 == 0)
-                        {
-                            // reverse? of course, since each time the process occurs
-                            // on the right (for simplicity)
-                            charValue = this.reverseBits(charValue);
-
-                            // can only be 0 if it is the stop character (the 8 zeros)
-                            if (charValue == 0)
-                            {
-                                this.ExtractedText = extractedText;
-                                return;
-                            }
-
-                            // convert the character value from int to char
-                            var c = (char) charValue;
-
-                            // add the current character to the result text
-                            extractedText += c.ToString();
-                        }
-                    }
-                }
-            }
-            this.ExtractedText = extractedText;
+            this.ExtractedText = this.decodeMessageInImage((Bitmap) this.EncodedImage);
         }
 
         #endregion
 
-        private int reverseBits(int n)
+        private string decodeMessageInImage(Bitmap bm)
         {
-            var result = 0;
+            // Initialize a random number generator.
+            var rand = new Random(1234);
 
-            for (var i = 0; i < 8; i++)
+            // Create a new HashSet.
+            var usedPositions = new HashSet<string>();
+
+            // Make a byte array big enough to hold the message length.
+            var len = 0;
+            var bytes = BitConverter.GetBytes(len);
+
+            // Decode the message length.
+            for (var i = 0; i < bytes.Length; i++)
             {
-                result = result*2 + n%2;
+                bytes[i] = this.decodeByte(bm, rand, usedPositions);
+            }
+            len = BitConverter.ToInt32(bytes, 0);
 
-                n /= 2;
+            // Sanity check.
+            if (len > 10000)
+            {
+                throw new InvalidDataException(
+                    "Message length " + len +
+                    " is too big to make sense. Invalid password.");
             }
 
-            return result;
+            // Decode the message bytes.
+            var chars = new char[len];
+            for (var i = 0; i < chars.Length; i++)
+            {
+                chars[i] = (char) this.decodeByte(bm, rand, usedPositions);
+            }
+            return new string(chars);
+        }
+
+        // Decode a byte.
+        private byte decodeByte(Bitmap bm, Random rand, HashSet<string> usedPositions)
+        {
+            byte value = 0;
+            byte valueMask = 1;
+            for (var i = 0; i < 8; i++)
+            {
+                // Find the position for the ith bit.
+                int row, col, pix;
+                this.pickPosition(bm, rand, usedPositions, out row, out col, out pix);
+
+                // Get the color component value.
+                byte colorValue = 0;
+                switch (pix)
+                {
+                    case 0:
+                        colorValue = bm.GetPixel(row, col).R;
+                        break;
+                    case 1:
+                        colorValue = bm.GetPixel(row, col).G;
+                        break;
+                    case 2:
+                        colorValue = bm.GetPixel(row, col).B;
+                        break;
+                }
+
+                // Set the next bit if appropriate.
+                if ((colorValue & 1) == 1)
+                {
+                    // Set the bit.
+                    value = (byte) (value | valueMask);
+                }
+
+                // Move to the next bit.
+                valueMask <<= 1;
+            }
+
+            return value;
+        }
+
+        private void pickPosition(Bitmap bm, Random rand,
+            HashSet<string> usedPositions,
+            out int row, out int col, out int pix)
+        {
+            for (;;)
+            {
+                // Pick random r, c, and pix.
+                row = rand.Next(0, bm.Width);
+                col = rand.Next(0, bm.Height);
+                pix = rand.Next(0, 3);
+
+                // See if this location is available.
+                var key =
+                    row + "/" +
+                    col + "/" +
+                    pix;
+                if (!usedPositions.Contains(key))
+                {
+                    usedPositions.Add(key);
+                    return;
+                }
+            }
         }
     }
 }
