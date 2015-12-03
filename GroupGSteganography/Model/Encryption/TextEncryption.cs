3a8ee52e8c84e6272fae79c78b8cfbcdb5d9915c
Matt Maginniss <maginniss.matt@gmail.com>
2015-12-03 01:08:50 -0500

    Created TextEncryption class.

---
+++ GroupGSteganography/Model/Encryption/TextEncryption.cs
@@ -0,0 +1,28 @@
+﻿namespace GroupGSteganography.Model.Encryption
+{
+    internal class TextEncryption
+    {
+        #region Properties
+
+        private string UnencryptedText { get; set; }
+        private int RotationValue { get; set; }
+        public string EncryptedText { get; private set; }
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="TextEncryption" /> class.
+        /// </summary>
+        /// <param name="unencryptedText">The unencrypted text.</param>
+        /// <param name="rotValue">The rot value.</param>
+        public TextEncryption(string unencryptedText, int rotValue)
+        {
+            this.UnencryptedText = unencryptedText;
+            this.RotationValue = rotValue;
+        }
+
+        #endregion
+    }
+}
