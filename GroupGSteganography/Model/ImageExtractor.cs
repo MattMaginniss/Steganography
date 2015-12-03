ddc521cd698862cb91a8d6b90a403edef765f1c8
Matt Maginniss <maginniss.matt@gmail.com>
2015-12-02 20:21:29 -0500

    Created an ImageExtrator class.

---
+++ GroupGSteganography/Model/ImageExtractor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupGSteganography.Model
+{
+    class ImageExtractor : IExtractor
+    {
+        public void Extract()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
