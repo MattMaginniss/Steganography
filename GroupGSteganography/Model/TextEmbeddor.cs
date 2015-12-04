e097bd40e3f814daf7d7b1f0edd614bcf766dfb7
Matt Maginniss <maginniss.matt@gmail.com>
2015-12-04 05:42:34 -0500

    Refactored embedding code to have minimally nested methods.

--- GroupGSteganography/Model/TextEmbeddor.cs
+++ GroupGSteganography/Model/TextEmbeddor.cs
@@ -59,11 +59,8 @@ namespace GroupGSteganography.Model
         /// <returns>the image with embedded text.</returns>
         public Image Embed()
         {
-            if (this.HeaderPixel.IsEncrypted)
-            {
-                var encrypter = new TextEncryption(this.MessageText, this.HeaderPixel.RotShift);
-                this.MessageText = encrypter.EncryptText();
-            }
+            this.checkForEncryption();
+
             var img = new Bitmap(this.SourceImage);
 
             for (var i = 0; i < img.Width; i++)
@@ -74,29 +71,57 @@ namespace GroupGSteganography.Model
                     {
                         var pixel = img.GetPixel(i, j);
 
-                        if (i*img.Width + j <= this.MessageText.Length)
-                        {
-                            var letter = Convert.ToChar(this.MessageText.Substring((i * img.Width + j) - 1, 1));
-                            var value = Convert.ToInt16(letter);
-
-                            img.SetPixel(i, j, Color.FromArgb(pixel.R, pixel.G, value));
-                        }
-
-                        if (this.MessageText.Length < (i*img.Width) + j &&
-                            (i*img.Width) + j <= this.MessageText.Length + 3)
-                        {
-                            var value = Convert.ToInt16('#');
-                            img.SetPixel(i, j, Color.FromArgb(pixel.R, pixel.G, value));
-                        }
+                        this.embedHiddenMessageCharacter(i, img, j, pixel);
+                        this.setEndOfMessageCharacters(i, img, j, pixel);
                     }
                     else
                     {
-                        img.SetPixel(0, 0, this.HeaderPixel.GetColor());
+                        this.embedHeaderPixel(img);
                     }
                 }
             }
 
             return img;
+        }
+
+        private void checkForEncryption()
+        {
+            if (this.HeaderPixel.IsEncrypted)
+            {
+                this.encryptMessage();
+            }
+        }
+
+        private void encryptMessage()
+        {
+            var encrypter = new TextEncryption(this.MessageText, this.HeaderPixel.RotShift);
+            this.MessageText = encrypter.EncryptText();
+        }
+
+        private void embedHeaderPixel(Bitmap img)
+        {
+            img.SetPixel(0, 0, this.HeaderPixel.GetColor());
+        }
+
+        private void embedHiddenMessageCharacter(int i, Bitmap img, int j, Color pixel)
+        {
+            if (i*img.Width + j <= this.MessageText.Length)
+            {
+                var letter = Convert.ToChar(this.MessageText.Substring((i*img.Width + j) - 1, 1));
+                var value = Convert.ToInt16(letter);
+
+                img.SetPixel(i, j, Color.FromArgb(pixel.R, pixel.G, value));
+            }
+        }
+
+        private void setEndOfMessageCharacters(int i, Bitmap img, int j, Color pixel)
+        {
+            if (this.MessageText.Length < (i*img.Width) + j &&
+                (i*img.Width) + j <= this.MessageText.Length + 3)
+            {
+                var value = Convert.ToInt16('#');
+                img.SetPixel(i, j, Color.FromArgb(pixel.R, pixel.G, value));
+            }
         }
 
         #endregion
